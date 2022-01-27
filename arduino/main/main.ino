647b825786c349c7527ec4d067fa790404bbbfdd
Zakaria EL KHAYDER <zelkhayder@gmail.com>
2022-01-27 03:05:39 +0100

    Reuploaded Arduino folder

---
+++ arduino/main/main.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h> 
+
+SoftwareSerial BTSerial(2, 3); // TXD | RXD 
+
+void setup() {
+  BTSerial.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(BTSerial.available()) {
+    Serial.write(BTSerial.read());
+  }
+
+  if(Serial.available()) {
+    BTSerial.write((char) Serial.read());
+  }
+}
