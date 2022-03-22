9fc6a831e64484f0c1d100f19749589082f9d0c0
EL KHAYDER Zakaria <zelkhayder@gmail.com>
2022-03-22 16:15:59 +0100

    Added Startup ringtone test

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "libraries/pitches.h"
 
 #include "./services/Bluetooth.cpp"
 
@@ -16,6 +17,7 @@ void setup()
     BatteryReader::setup();
     LeftButton::setup();
 
+    onStart();
     // delay(1000);
 }
 
@@ -26,4 +28,54 @@ void loop()
     ProximityVibrator::loop();
     // BatteryReader::loop();
     LeftButton::loop();
+}
+
+void onStart()
+{
+    int melody[] = {
+        NOTE_C4,
+        NOTE_G3,
+        NOTE_G3,
+        NOTE_A3,
+        NOTE_G3,
+        0,
+        NOTE_B3,
+        NOTE_C4,
+    };
+
+    // note durations: 4 = quarter note, 8 = eighth note, etc.:
+    int noteDurations[] = {
+        4,
+        8,
+        8,
+        4,
+        4,
+        4,
+        4,
+        4,
+    };
+
+    for (int thisNote = 0; thisNote < 8; thisNote++)
+    {
+
+        // to calculate the note duration, take one second divided by the note type.
+
+        // e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+
+        int noteDuration = 1000 / noteDurations[thisNote];
+
+        tone(8, melody[thisNote], noteDuration);
+
+        // to distinguish the notes, set a minimum time between them.
+
+        // the note's duration + 30% seems to work well:
+
+        int pauseBetweenNotes = noteDuration * 1.30;
+
+        delay(pauseBetweenNotes);
+
+        // stop the tone playing:
+
+        noTone(8);
+    }
 }
